28455b0392ac320e45fc2f57bf64958d20ad3ea4
d.sokolov <d.sokolov@ailove.ru>
2015-06-25 14:17:01 +0300

    fix version

--- setup.py
+++ setup.py
@@ -1,4 +1,24 @@
-from distutils.core import setup
+import os
+
+from setuptools import setup
+
+
+def get_packages(package):
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+def get_package_data(package):
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
 
 setup(
     name='django-love-utils',
@@ -8,7 +28,8 @@ setup(
     author='Ailove',
     author_email='ailove@ailove.com',
     description='Utils for django project',
-    packages=['love_utils'],
+    packages=get_packages('love_utils'),
+    package_data=get_package_data('love_utils'),
     zip_safe=False,
     include_package_data=True,
     platforms='any',
